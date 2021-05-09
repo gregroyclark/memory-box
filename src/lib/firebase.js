573f39739a1758ae9897d641df7ff8445f6c634b
gregroyclark <gregroyclark@gmail.com>
2021-05-09 16:26:56 -0500

    added firebase config values, seeded database (and comment out seed function for safety)

---
+++ src/lib/firebase.js
@@ -0,0 +1,21 @@
+import Firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+import { seedDatabase } from "../seed";
+
+const config = {
+  apiKey: "AIzaSyBJgeF_GFt5eWJeZCShQMBaCW2KwQCTFjM",
+  authDomain: "memory-box-92bbc.firebaseapp.com",
+  projectId: "memory-box-92bbc",
+  storageBucket: "memory-box-92bbc.appspot.com",
+  messagingSenderId: "178301832482",
+  appId: "1:178301832482:web:0113c567a960805897e97b",
+};
+
+const firebase = Firebase.initializeApp(config);
+const { FieldValue } = Firebase.firestore;
+
+// seedDatabase(firebase);
+
+export { firebase, FieldValue };
